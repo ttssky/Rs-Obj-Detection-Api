fb1108cca9024f55933ed7bb2c9d031b304a5112
ttssky <lvyikai1991@163.com>
2021-04-08 15:02:18 +0800

    modify readme.md

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from config import config
 from flask_cors import CORS
-from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
-# ma = Marshmallow()
 cors = CORS()
 db = SQLAlchemy()
 
